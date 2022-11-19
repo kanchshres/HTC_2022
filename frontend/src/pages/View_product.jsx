e6ef0c6c9b7417be80f94c5d9a83e58c0c122680
Maziyar Dowlatabadibazaz <maziyardowlat@gmail.com>
2022-11-19 15:29:28 -0700

    Maziyar commit

--- frontend/src/pages/View_product.jsx
+++ frontend/src/pages/View_product.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import blazer from "../assets/blazer.png";
+import './cards.css';
 
 const ViewProduct = () => {
   return (
-    <div>
-        <p>Here is our view products page</p>
+  <div className='cards blue lighten-3'>
+      <h1>
+        Current Products on Hold
+      </h1>
+    <div className="cards__container">
+      <div class="row">
+        <div class="col m6">
+          <div class="card">
+            <div class="card-image">
+              <img src={blazer}/>
+              <span class="card-title">Blazer</span>
+            </div>
+            <div class="card-content">
+              <p>A blazer, medium sized  
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
